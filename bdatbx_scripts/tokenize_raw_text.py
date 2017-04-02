099ac4a112a3987d00f9665d1dd5eb5ef9a05d78
Basti Tee <basti.tee@gmx.de>
2017-04-02 14:14:16 +0200

    made languages more extendable;

--- bdatbx_scripts/tokenize_raw_text.py
+++ bdatbx_scripts/tokenize_raw_text.py
@@ -26,12 +26,7 @@ from bptbx import b_iotools, b_threading
 from bdatbx import b_lists, b_util, b_mongo, b_const
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
-
-SUPPORTED_LANGS = {
-    'de': 'german',
-    'en': 'english',
-    'unk': 'unknown'
-}
+from sys import exit
 
 def tokenize(text_lines):
     tokens = []
@@ -55,18 +50,15 @@ def detect_language(tokens, doc=None):
     lang_db = b_mongo.get_key_nullsafe(doc, b_const.DB_LANG_AUTO)
     # if not try to decide between german and english
     if not lang_db:
-        if (len(b_lists.intersect(tokens, sw_en)) > len(
-                b_lists.intersect(tokens, sw_de))):
+        if (len(b_lists.intersect(tokens, SUPPORTED_LANGS['en']['sw'])) > len(
+                b_lists.intersect(tokens, SUPPORTED_LANGS['de']['sw']))):
             lang_db = 'en'
         else:
             lang_db = 'de'
-    if lang_db == 'de':
-        return 'de', sw_de, stem_de, stem_2_source_dict_de
-    elif lang_db == 'en':
-        return 'en', sw_en, stem_en, stem_2_source_dict_en
-    else:
-        return 'unk', sw_en, stem_en, stem_2_source_dict_unk
-
+    try:
+        return SUPPORTED_LANGS[lang_db]
+    except KeyError:
+        return SUPPORTED_LANGS['unk']
 
 def stem(tokens, stemmer, stem_2_source_dict):
     stemmed_tokens = []
@@ -94,9 +86,11 @@ def stopword_removal(tokens, stopwords):
     return filtered_tokens
 
 
-def write_dictionary_to_file(dictionary, filename):
+def write_dictionary_to_file(dictionary, stopwords, filename):
     sorted_dictionary = {}
     for dict_key in dictionary:  # for each stem in dictionary
+        if dict_key in stopwords:
+            continue  # don't
         dict_value = dictionary[dict_key]
         total_count = 0
         sorted_entries = []
@@ -127,9 +121,7 @@ def extend_stopwords_with_manual_list(stopwords, lang):
     stopwords += new_stopwords
     return stopwords
 
-
 def worker(in_file, col=None, doc=None):
-
     try:
         # when using mongo, check if word count is relevant
         if doc is not None:
@@ -150,9 +142,9 @@ def worker(in_file, col=None, doc=None):
         text_lines = b_iotools.read_file_to_list(in_file, True)
         tokens = tokenize(text_lines)
         tokens = remove_nonwords(tokens)
-        language, stopwords, stemmer, s2s_dict = detect_language(tokens, doc)
-        tokens = stem(tokens, stemmer, s2s_dict)
-        tokens = stopword_removal(tokens, stopwords)
+        lang_kit = detect_language(tokens, doc)
+        tokens = stem(tokens, lang_kit['stemmer'], lang_kit['s2s'])
+        tokens = stopword_removal(tokens, lang_kit['sw'])
         b_mongo.set_null_safe(doc, b_const.DB_TOK_WC, len(tokens))
         b_mongo.set_null_safe(doc, b_const.DB_TOK_TOKENS, tokens)
         # ------------------------------------------------
@@ -167,19 +159,29 @@ def worker(in_file, col=None, doc=None):
         b_util.update_progressbar()
 
 
-# setup NLTK
+# setup language tools
 if args.n:
     nltk.data.path.append(args.n)
     b_util.log('added nltk path {}'.format(args.n))
-stem_en = SnowballStemmer('english', ignore_stopwords=False)
-stem_de = SnowballStemmer('german', ignore_stopwords=False)
-sw_en = [stem_en.stem(sw) for sw in extend_stopwords_with_manual_list(
-    stopwords.words('english'), 'english')]
-sw_de = [stem_de.stem(sw) for sw in extend_stopwords_with_manual_list(
-    stopwords.words('german'), 'german')]
-stem_2_source_dict_de = {}
-stem_2_source_dict_en = {}
-stem_2_source_dict_unk = {}
+
+SUPPORTED_LANGS = {
+    'de': { 'name': 'german' },
+    'en': { 'name': 'english' },
+    'es': { 'name': 'spanish' },
+    'unk': { 'name': 'unknown'}
+}
+for iso, lang_kit in SUPPORTED_LANGS.items():
+    if iso == 'unk':
+        continue
+    lang = lang_kit['name']
+    lang_kit['stemmer'] = SnowballStemmer(lang, ignore_stopwords=False)
+    lang_kit['sw'] = [lang_kit['stemmer'].stem(sw) for sw in extend_stopwords_with_manual_list(
+        stopwords.words(lang), lang)]
+    lang_kit['s2s'] = {}
+    b_util.log('-- finished setup for language {}/{}'.format(iso, lang))
+SUPPORTED_LANGS['unk']['stemmer'] = SUPPORTED_LANGS['en']['stemmer']
+SUPPORTED_LANGS['unk']['sw'] = SUPPORTED_LANGS['en']['sw']
+SUPPORTED_LANGS['unk']['s2s'] = {}
 
 pool = b_threading.ThreadPool(args.t)
 if args.i and not col:
@@ -203,15 +205,10 @@ pool.wait_completion()
 b_util.finish_progressbar()
 
 # write out data points
-for lang in list(SUPPORTED_LANGS.keys()):
-    ofile = 'token_dictionary_{}.txt'.format(lang)
+for kit in list(SUPPORTED_LANGS.values()):
+    ofile = 'token_dict_{}.txt'.format(kit['name'])
     b_iotools.remove_silent(ofile)
-    if lang == 'de':
-        write_dictionary_to_file(stem_2_source_dict_de, ofile)
-    elif lang == 'en':
-        write_dictionary_to_file(stem_2_source_dict_en, ofile)
-    else:
-        write_dictionary_to_file(stem_2_source_dict_unk, ofile)
+    write_dictionary_to_file(kit['s2s'], kit['sw'], ofile)
 
 def main():
     pass
