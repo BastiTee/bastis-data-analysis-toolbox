9c8ef0dd4872ee7410bf25c6cf090eb2397f38ff
Basti Tee <basti.tee@gmx.de>
2017-04-02 19:41:12 +0200

    bugfix;

--- bdatbx_scripts/size_of_mongodb_collection.py
+++ bdatbx_scripts/size_of_mongodb_collection.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
+from sys import argv
 try:
-    collection_name=sys.argv[1]
+    collection_name=argv[1]
 except IndexError:
-    sys.exit(1)
+    print("No collection name given.")
+    exit(1)
 
 from bdatbx import b_mongo
 col = b_mongo.get_client_for_collection(collection_name, create=False)
 if col is None:
     print('0')
+    exit(1)
 size = b_mongo.get_collection_size(col)
 print(size)
 
