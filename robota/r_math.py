299b9ddf097fe9c0e776c64d29ad6b31e1b4f6a8
Basti Tee <basti.tee@gmx.de>
2017-05-03 23:12:58 +0200

    new stat methods;

--- robota/r_math.py
+++ robota/r_math.py
@@ -6,3 +6,38 @@ def round_ns(number, digits=0, default_return=None):
     if number is None:
         return default_return
     return round(number, digits)
+
+
+def count_and_sort(array, reverse=False, remove_outliers=False):
+    """Count the elements in the given array and sort by element."""
+    amap = {}
+    for el in array:
+        try:
+            amap[el]
+        except KeyError:
+            amap[el] = 0
+        amap[el] = amap[el] + 1
+
+    rej = _reject_outliers(list(amap.values()))
+    rejmin = min(rej)
+    rejmax = max(rej)
+    new_array = []
+    for sorted_key in sorted(amap.keys(), reverse=reverse):
+        count = amap[sorted_key]
+        if count < rejmin or count > rejmax:
+            continue
+        new_array.append([sorted_key, amap[sorted_key]])
+
+    return new_array
+
+
+def _reject_outliers(arr, m=2):
+    import numpy
+    elements = numpy.array(arr)
+
+    mean = numpy.mean(elements, axis=0)
+    sd = numpy.std(elements, axis=0)
+
+    final_list = [x for x in arr if (x > mean - m * sd)]
+    final_list = [x for x in final_list if (x < mean + m * sd)]
+    return final_list
